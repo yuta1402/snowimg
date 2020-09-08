a983c289ff0f9ede9fab206c13648bc23b329898
gut <yuta1402souja@live.jp>
2020-09-09 03:22:31 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mainConfig = {
+  target: "electron-main",
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  entry: {
+    main: "./src/main/main.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loaders: "ts-loader",
+      },
+    ],
+  },
+  devtool: "source-map",
+  watchOptions: {
+    poll: true,
+  },
+};
+
+const rendererConfig = {
+  target: "electron-renderer",
+  entry: {
+    index: "./src/renderer/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: true,
+              modules: true,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", "css, scss"],
+  },
+  devtool: "source-map",
+  watchOptions: {
+    poll: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundles.css",
+    }),
+  ],
+};
+
+module.exports = [mainConfig, rendererConfig];
